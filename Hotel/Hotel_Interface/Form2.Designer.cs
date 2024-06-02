d722ca3cce79df6286951c88de162923dfd7f5b3
Ostap4ik777 <137522973+Ostap4i@users.noreply.github.com>
2024-06-02 09:01:44 +0300

    Add files via upload

---
+++ Hotel/Hotel_Interface/Form2.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Hotel_Interface
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            toolTip = new ToolTip(components);
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // button1
+            // 
+            button1.Location = new Point(37, 112);
+            button1.Name = "button1";
+            button1.Size = new Size(127, 27);
+            button1.TabIndex = 1;
+            button1.Text = "435";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(193, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(158, 40);
+            button2.TabIndex = 2;
+            button2.Text = "1";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(376, 12);
+            button3.Name = "button3";
+            button3.Size = new Size(158, 40);
+            button3.TabIndex = 3;
+            button3.Text = "55";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.CausesValidation = false;
+            button4.Location = new Point(563, 12);
+            button4.Name = "button4";
+            button4.Size = new Size(158, 40);
+            button4.TabIndex = 4;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(193, 112);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(158, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(978, 529);
+            Controls.Add(textBox1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form2";
+            Text = "Form2";
+            Load += Form2_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ContextMenuStrip contextMenuStrip1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private ToolTip toolTip;
+        private TextBox textBox1;
+    }
+}
